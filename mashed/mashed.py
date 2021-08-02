05ec235c0ccf141214086c76f72f1eff6cd6432f
account <apaluza3@gmail.com>
2021-08-02 13:15:22 +0300

    Mashed

--- mashed/mashed.py
+++ mashed/mashed.py
@@ -7,6 +7,11 @@ cls_iter = 0
 atrib = [""] * 1000;
 cls = [""] * 1000;
 
+atrib_val = [""] * 1000;
+cls_val = [""] * 1000;
+
+
+
 filename = raw_input( "Filename: " );
 
 def get_lines( filename ):
@@ -18,19 +23,30 @@ def get_lines( filename ):
 
 def get_file( filename ):
 	with open( filename, 'r') as file:
-		data = str( file.read().replace('\n', '') )
+		data = str( file.read().replace( '\n', '' ) )
 	return data
 
+def print_array( arr ):
+	for i in range( len( arr ) ):
+		if arr[ i ] == "":
+			break
+		print( arr[ i ] )
+
+def had( arr, pos ):
+	for i in range( pos ):
+		if arr[ pos ] == arr[ i ]:
+			return 1
+	return 0
 
 data = get_file( filename );
-for i in range( len( filename ) ):
+for i in range( len( data ) ):
 	if data[ i ] == '[':
+		i = i + 1
 		while data[ i ] == ' ' or data[ i ] == '	':
 			i = i + 1
 		
 		while data[ i ] != ' ' and data[ i ] != '	':
 			atrib[ atrib_iter ] = atrib[ atrib_iter ] + data[ i ]
-			print( data[ i ] + ' ' )
 			i = i + 1
 		
 		while data[ i ] == ' ' or data[ i ] == '	':
@@ -38,10 +54,59 @@ for i in range( len( filename ) ):
 
 		while data[ i ] == ']':
 			i = i + 1
-		i = i + 1
 
-		print( atrib[ atrib_iter ] )
+		if data[ i ] == '{':
+			while 1:
+				i = i + 1
+				if data[ i ] == '}':
+					break
+				atrib_val[ atrib_iter ] = atrib_val[ atrib_iter ] + data[ i ]
+
 		atrib_iter = atrib_iter + 1;
 
+	if data[ i ] == '.':
+		i = i + 1
+		while data[ i ] != ' ' and data[ i ] != '	' and data[ i ] != '{':
+			cls[ cls_iter ] = cls[ cls_iter ] + data[ i ]
+			i = i + 1
+	
+		if i <= len( data ) - 1:
+			while data[ i ] != '{':
+				i = i + 1
 
-		
+		while 1:
+			i = i + 1
+			if data[ i ] == '}':
+				break
+			cls_val[ cls_iter ] = cls_val[ cls_iter ] + data[ i ]
+
+		cls_iter = cls_iter + 1;
+
+
+
+fo = open( "result", "w+" )
+
+def print_atrib( atrb ):
+	result = "[ " + atrb + " ]\n{\n"
+	for i in range( atrib_iter ):
+		if atrib[ i ] == atrb:
+			result = result + atrib_val[ i ] + "\n"
+	result = result + "}\n\n";
+	return result
+
+def print_cls( cl ):
+	result = "." + cl + "\n{\n"
+	for i in range( cls_iter ):
+		if cls[ i ] == cl:
+			result = result + cls_val[ i ] + "\n"
+	result = result + "}\n\n";
+	return result
+
+for i in range( atrib_iter ):
+	if had( atrib, i ) == 0:
+		fo.write( print_atrib( atrib[ i ] ) )
+
+for i in range( cls_iter ):
+	if had( cls, i ) == 0:
+		fo.write( print_cls( cls[ i ] ) )
+
