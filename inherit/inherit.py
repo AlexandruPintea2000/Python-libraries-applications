f57ac5a8a55a83c214380551848aa9fa5cdb7190
account <apaluza3@gmail.com>
2021-08-02 16:12:35 +0300

    Inherit

--- inherit/inherit.py
+++ inherit/inherit.py
@@ -1,9 +1,89 @@
 x = 4
 print(x)
 
-fi = open( "file", "r+" )
-data = fi.read()
-fi.close()
+atrib = [""] * 1000
+val = [""] * 1000
+atrib_iter = 0
 
-for i in range( len( data ) ):
-	if data[ i ] == '':
+def get_lines( filename ):
+	with open( filename ) as f:
+		lines = f.readlines()
+	for i in lines:
+		i = i[:len( i ) - 1]
+	return lines
+
+filename = "file";
+lines = get_lines( filename );
+lines_iter = len( lines );
+for i in range( lines_iter ):
+	if lines[ i ][ 0 ] == '\\':
+		l = 1
+		while lines[ i ][ l ] != ':' and lines[ i ][ l ] != '{':
+			atrib[ atrib_iter ] = atrib[ atrib_iter ] + lines[ i ][ l ]
+			if ( l == len( lines[ i ] ) - 1 ):
+				break
+			l = l + 1
+
+		atrib[ atrib_iter ] = str.strip( atrib[ atrib_iter ] )
+
+		atrib_iter = atrib_iter + 1
+
+for i in atrib:
+	if ( i == "" ):
+		break
+	print( i )
+
+def get_file( filename ):
+	with open( filename, 'r') as file:
+		data = str( file.read().replace( '\n', '' ) )
+	return data
+
+def get_content( attr ):
+	data = get_file( filename );	
+	itr = str.find( data, "\\" + attr ) + len( attr ) + 1
+
+	while data[ itr ] != '{':
+		itr = itr + 1
+	itr = itr + 1
+	
+	atr_iter = 0
+	for i in range( atrib_iter ):
+		if ( atrib[ i ] == attr ):
+			atr_iter = i
+	while itr != len( data ) and data[ itr ] != '}':
+		val[ atr_iter ] = val[ atr_iter ] + data[ itr ]
+		itr = itr + 1
+
+	return val[ atr_iter ]
+
+def get_inheritants( attr ):
+	data = get_file( filename );	
+	itr = str.find( data, "\\" + attr ) + len( attr ) + 1
+
+	if data[ itr ] != ':':
+		return []
+
+	itr = itr + 1;
+
+	inhr_string = ""
+	while data[ itr ] != '{':
+		inhr_string = inhr_string + data[ itr ]
+		itr = itr + 1
+
+	inhr_string = str.strip( inhr_string )
+	inhr = inhr_string.split( " " )
+
+	return inhr
+
+
+f = open( "result", "w+" )
+for i in range( atrib_iter ):
+	f.write( "[ atr = " + atrib[ i ] + " ]\n{\n" );
+	inhr = get_inheritants( atrib[ i ] )
+	for l in range( len( inhr ) ):
+		f.write( get_content( inhr[ l ] ) + '\n' )
+	f.write( get_content( atrib[ i ] ) + '\n' )
+	f.write( "}\n\n" )
+	
+f.close()
+
